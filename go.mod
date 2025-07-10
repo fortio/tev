43ea119bbae975d28c7c9723eb908dc7f56c1be4
Laurent Demailly <ldemailly@gmail.com>
2025-07-10 11:39:37 -0700

    mouse tracking

--- go.mod
+++ go.mod
@@ -1,11 +1,11 @@
 module fortio.org/tev
 
-go 1.23.8
+go 1.23.0
 
 require (
 	fortio.org/cli v1.10.0
 	fortio.org/log v1.17.2
-	fortio.org/terminal v0.37.0
+	fortio.org/terminal v0.37.1-0.20250710182632-2501b2dd3323
 )
 
 require (
@@ -15,7 +15,7 @@ require (
 	github.com/kortschak/goroutine v1.1.2 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
 	golang.org/x/crypto/x509roots/fallback v0.0.0-20250406160420-959f8f3db0fb // indirect
-	golang.org/x/image v0.27.0 // indirect
-	golang.org/x/sys v0.33.0 // indirect
-	golang.org/x/term v0.32.0 // indirect
+	golang.org/x/image v0.29.0 // indirect
+	golang.org/x/sys v0.34.0 // indirect
+	golang.org/x/term v0.33.0 // indirect
 )
--- tev.go
+++ tev.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"os"
 
 	"fortio.org/cli"
@@ -13,21 +14,35 @@ func main() {
 }
 
 func Main() int {
+	noMouseFlag := flag.Bool("no-mouse", false, "Disable mouse tracking events (default is to enable it)")
 	cli.Main()
 	ap := ansipixels.NewAnsiPixels(20.)
 	err := ap.Open()
 	if err != nil {
 		return log.FErrf("Failed to open terminal: %v", err)
 	}
-	defer ap.Restore()
+	defer func() {
+		ap.MoveCursor(0, ap.H-1)
+		ap.MouseTrackingOff()
+		ap.MouseClickOff()
+		ap.Restore()
+	}()
 	ap.OnResize = func() error {
 		log.Infof("Terminal resized to %dx%d\r", ap.W, ap.H)
 		return nil
 	}
+	ap.NoDecode = true // we handle mouse events ourselves
+	if !*noMouseFlag {
+		ap.MouseTrackingOn()
+		ap.Out.Flush()
+		log.Infof("Mouse tracking enabled\r")
+	} else {
+		log.Infof("Mouse tracking disabled\r")
+	}
 	cont := 3
 	// TODO plug the writer that auto connverts \n to \r\n
 	log.Infof("Fortio terminal event dump started. ^C 3 times to exit (or pkill tev)\r")
-	for cont > 0 {
+	for {
 		err = ap.ReadOrResizeOrSignal()
 		if err != nil {
 			return log.FErrf("Error reading terminal: %v", err)
@@ -37,13 +52,21 @@ func Main() int {
 			continue
 		}
 		log.Infof("Read %d bytes: %q\r", len(ap.Data), ap.Data)
+		ap.MouseDecode()
+		if ap.Mouse {
+			log.Infof("Mouse event detected: buttons %06b, x %d, y %d\r", ap.Mbuttons, ap.Mx, ap.My)
+			continue
+		}
 		if ap.Data[0] == 3 { // Ctrl-C
 			cont--
+			if cont == 0 {
+				log.Infof("3rd Ctrl-C received, exiting now.\r")
+				return 0
+			}
 			log.Infof("Ctrl-C received, %d more to exit..\r", cont)
 		} else {
 			cont = 3 // reset count on any other input
 		}
 	}
-	ap.MoveCursor(0, ap.H-1)
 	return 0
 }
