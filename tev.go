93a16479fb628a398719cfdec1d5bdf886f8f3ce
Laurent Demailly <ldemailly@gmail.com>
2025-07-26 19:54:29 -0700

    Add \n in -echo mode when \r is read. Split functions

--- tev.go
+++ tev.go
@@ -36,6 +36,16 @@ func GetTabStops(ap *ansipixels.AnsiPixels) []int {
 	return tabs
 }
 
+func Restore(ap *ansipixels.AnsiPixels) {
+	ap.MoveCursor(0, ap.H-1)
+	ap.MousePixelsOff()
+	ap.MouseX10Off()
+	ap.MouseTrackingOff()
+	ap.MouseClickOff()
+	ap.SetBracketedPasteMode(false)
+	ap.Restore()
+}
+
 func Main() int {
 	noMouseFlag := flag.Bool("no-mouse", false, "Disable mouse tracking events (enabled by default)")
 	mousePixelsFlag := flag.Bool("mouse-pixels", false, "Enable mouse pixel events (vs grid)")
@@ -58,26 +68,14 @@ func Main() int {
 		log.LogVf("Not enabling raw mode, staying in cooked mode")
 		_ = ap.GetSize() // to set ah.H for restore.
 	}
-	defer func() {
-		// do it even in cooked mode to turn off mouse spam etc...
-		ap.MoveCursor(0, ap.H-1)
-		ap.MousePixelsOff()
-		ap.MouseX10Off()
-		ap.MouseTrackingOff()
-		ap.MouseClickOff()
-		ap.SetBracketedPasteMode(false)
-		ap.Restore()
-	}()
+	// do it even in cooked mode to turn off mouse spam etc...
+	defer Restore(ap)
 	ap.OnResize = func() error {
 		log.Infof("Terminal resized to %dx%d", ap.W, ap.H)
 		return nil
 	}
 	ap.NoDecode = true // we handle mouse events ourselves
 	echoMode := *echoFlag
-	logLevel := log.Info
-	if echoMode {
-		logLevel = log.Verbose
-	}
 	if !echoMode && !*noMouseFlag {
 		ap.MouseTrackingOn()
 		log.Infof("Mouse tracking enabled")
@@ -99,13 +97,21 @@ func Main() int {
 		log.Infof("Bracketed paste mode disabled")
 	}
 	ap.Out.Flush()
-	exitCount := 3
 	log.Infof("Fortio terminal event dump started. ^C 3 times to exit (or pkill tev). Ctrl-L clears the screen.")
 	if !*noRawFlag {
 		log.Infof("Tabs: %v", GetTabStops(ap))
 	} else {
 		log.Infof("Sample tabs:\n\t0\t1\t2\t3\t4\t5\t6\t7\t8")
 	}
+	return DebugLoop(ap, echoMode)
+}
+
+func DebugLoop(ap *ansipixels.AnsiPixels, echoMode bool) int {
+	logLevel := log.Info
+	if echoMode {
+		logLevel = log.Verbose
+	}
+	exitCount := 3
 	for {
 		err := ap.ReadOrResizeOrSignal()
 		if err != nil {
@@ -133,6 +139,10 @@ func Main() int {
 				return 0
 			}
 			log.Infof("Ctrl-C received, %d more to exit..", exitCount)
+		case '\r':
+			if echoMode {
+				os.Stdout.Write([]byte{'\n'}) // echo \r as \n
+			}
 		case '\f': // Ctrl-L
 			log.Infof("Ctrl-L received, clearing screen.")
 			ap.ClearScreen()
